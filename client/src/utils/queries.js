4fd3dadfe8f05fba514967545d53edd7b5d8caa4
DannyM15 <merinodanny15@gmail.com>
2024-05-23 18:33:39 -0700

    big update

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_ME = gql `
+query Me {
+    me {
+      savedBooks {
+        Link
+        _id
+        authors
+        bookId
+        description
+        image
+        title
+      }
+    }
+  }
+
+
+`
